b13fe4d43391de315b1c970621a1a7f2ac3f6e5c
wallace <willfs144@gmail.com>
2016-05-19 12:01:43 -0500

    Leer parametros

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require("express");
-
-
+var bodyParser = require("body-parser");
 var app = express(); // tomamos el objeto
 
-//archivos staticos css
-app.use("/estatico",express.static('public'));//middlewares
+
+
+//middlewares
+app.use("/estatico",express.static('public'));//archivos staticos css
+app.use(bodyParser.json());// para peticiones application/json
+app.use(bodyParser.urlencoded({extended: true}));// parsear tambien arreglos
 app.use(express.static('assets'));//middlewares
-
-
-
 
 app.set("view engine", "jade");//implementa jade
 
@@ -16,8 +16,14 @@ app.get("/", function(solicitud, respuesta){
 	respuesta.render("index");
 });
 
-app.get("/login", function(solicitud,respuesta){
+app.get("/login", function(solicitud,respuesta){	
 	respuesta.render("login");
-})
+});
+
+app.post("/users", function(solicitud, respuesta){
+	console.log("Contraseña: "+solicitud.body.pass);
+	console.log("Email: "+solicitud.body.email);
+	respuesta.send("Recibimos login");
+});
 
 app.listen(8080);
