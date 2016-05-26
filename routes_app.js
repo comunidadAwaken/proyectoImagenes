bfa78905f3f1e5c32fae76d4317e6d3731640af1
Wallace144 <willfs144@gmail.com>
2016-05-25 22:24:04 -0500

    Rutas para CRUD tipo Rest

--- routes_app.js
+++ routes_app.js
@@ -6,4 +6,45 @@ router.get("/", function(solicitud, respuesta){
 	respuesta.render("app/home");
 });
 
+/* REST */
+
+//muestra formulario para agregar
+router.get("/imagenes/new", function(solicitud,respuesta){
+	respuesta.render("app/imagenes/new");
+
+});
+
+// muestra formulario para editar
+router.get("/imagenes/:id/edit", function(solicitud,respuesta){
+
+});
+
+
+router.route("/imagenes/:id")
+	//mostrar o tomar
+	.get(function(solicitud, respuesta){
+
+	})
+	//actualizar 
+	.put(function(solicitud, respuesta){
+
+	})
+	//eliminar foto
+	.delete(function(solicitud,respuesta){
+
+	});
+
+router.route("/imagenes")
+	//mostrar o tomar
+	.get(function(solicitud, respuesta){
+
+	})
+	//crear una nueva imagen 
+	.post(function(solicitud, respuesta){
+
+	})
+	
+
+
+
 module.exports = router;
